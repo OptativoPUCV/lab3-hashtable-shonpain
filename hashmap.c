68f2b3e4b0c27688352ef2441f91f66e152d66ae
Replit user <>
2023-09-08 00:28:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -106,7 +106,7 @@ Pair * searchMap(HashMap * map,  char * key) {
         }
         // Avanzar al siguiente índice 
         position = (position + 1) % map->capacity;
-        if (position == original_position)return NULL;
+        if (position == orPositionosition)return NULL;
     }
 
     // La clave no se encontró 
